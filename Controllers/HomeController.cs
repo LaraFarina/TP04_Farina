af03a0eb47bfb4ebb8b54533668b62caa4970cc6
LARA MICAELA FARINA <47871657@ort>
2023-05-08 09:49:51 -0300

    HomeController en proceso

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,15 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
+        ViewBag.Paquete = ORTWorld.Paquetes;
         return View();
+    }
+    public IActionResult SelectPaquete(){
+        ViewBag.Destinos = ORTWorld.ListaDestinos;
+        ViewBag.Hoteles = ORTWorld.ListaHoteles;
+        ViewBag.Excursiones = ORTWorld.ListaExcursiones;
+        ViewBag.Aereos = ORTWorld.ListaAereos;
+        return View("Index");
+    
     }
 }
