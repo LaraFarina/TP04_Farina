110aeac7b268d01d7f1422a4b23263fff1e783a1
LARA MICAELA FARINA <47871657@ort>
2023-05-08 08:09:56 -0300

    Creacion del proyecto

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TP04_Farina.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
