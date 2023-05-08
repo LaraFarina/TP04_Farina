7b01a0721d9b51edd52f6583598b579bee1a5d09
LARA MICAELA FARINA <47871657@ort>
2023-05-08 08:52:40 -0300

    Datos precargados terminados

---
+++ Models/ORTWorld.cs
@@ -0,0 +1,18 @@
+class ORTWorld{
+    public static List<string> ListaDestinos {get; private set;} = new List<string>{"Berlin, Madrid, Barcelona, Viena, Florida, Miami, Seoul, Valencia, Londres, Manchester"};
+    public static List<string> ListaHoteles {get; private set;} = new List<string>{"hotelBerlin.jfif, hotelMadrid.jpg, hotelBarcelona.jpg, hotelViena.jpg, hotelFlorida.jpg, hotelMiami.jpg, hotelSeoul.jpg, hotelValencia.jpg, hotelLondres.jpg, hotelManchester.jpg"};
+    public static List<string> ListaAereos {get; private set;} = new List<string>{"vueling.jpg, americanairlines.jpg, qatar.jpg, airfrance.jpg, aireuropa.jpg, latam.jpg, flybondi.jpg, iberia.png, aerolineasargentinas.jpg, avianca.jpg"}; 
+    public static List<string> ListaExcursiones {get; private set;} = new List<string>{"ex1.jpg, ex2.jpg, ex3.jpg, ex4.jpg, ex5.jfif, ex6.jpg, ex7.jpg, ex8.jpg, ex9.jpg, ex10.jpg"};
+    public static Dictionary<string, Paquete> Paquetes {get; private set;}
+
+ 
+
+public static bool IngresarPaquete(string destinoSeleccionado, Paquete paquete){
+bool coincide = false;
+if(ListaDestinos.Contains(destinoSeleccionado) == true){
+coincide = true;
+}
+
+return coincide;
+}
+}
--- Models/Paquete.cs
+++ Models/Paquete.cs
@@ -1,8 +1,8 @@
 class Paquete
 {
     public string Hotel { get; private set; } 
-    public string Aereo { get; set; } 
-    public string Excursion { get; set; } 
+    public string Aereo { get; private set; } 
+    public string Excursion { get; private set; } 
 
     public Paquete(string hotel, string aereo, string excursion){
         hotel = Hotel;
